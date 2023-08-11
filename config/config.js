a7dd3d6f20b9178ed33b1f3d5ce4dd8194eba5bb
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-11 21:51:44 +0300

    feat: add host and port variables for redis connection

--- config/config.js
+++ config/config.js
@@ -5,4 +5,6 @@ module.exports = {
   DB_URI: process.env.DB_URI || "mongodb://localhost:27017/smoothie-recipes",
   ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || "*",
   NODE_ENV: process.env.NODE_ENV || "Development",
+  REDIS_HOST: process.env.REDIS_HOST || "127.0.0.1",
+  REDIS_PORT: process.env.REDIS_PORT || 6379,
 };
