ac3b894d745a9c32fc3de0536b28d53ce2acb8a2
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-11 17:40:18 +0300

    feat: create auth routes with documents

---
+++ routes/auth.js
@@ -0,0 +1,139 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ */
+const router = require("express").Router();
+
+const authController = require("../controllers/auth");
+const authValidator = require("../validators/auth");
+
+/**
+ * @swagger
+ * /auth/signup:
+ *   post:
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: email@google.com
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: 123456
+ *     responses:
+ *       201:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       422:
+ *         description: Unprocessable Entity
+ *       500:
+ *         description: Internal Server Error
+ */
+router.post("/signup", authValidator.signup, authController.signup);
+
+/**
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: email@google.com
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: 123456
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Internal Server Error
+ */
+router.post("/signin", authValidator.signin, authController.signin);
+
+/**
+ * @swagger
+ * /auth/signout:
+ *   delete:
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *                 format: refreshToken
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Internal Server Error
+ */
+router.delete("/signout", authValidator.signout, authController.signout);
+
+/**
+ * @swagger
+ * /auth/refreshToken:
+ *   post:
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *                 format: refreshToken
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Internal Server Error
+ */
+router.post(
+  "/refreshToken",
+  authValidator.refreshToken,
+  authController.refreshToken
+);
+
+module.exports = router;
