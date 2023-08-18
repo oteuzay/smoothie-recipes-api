fa7041bd4045757f68c9c7f803d9e704b5854bcf
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-19 00:23:08 +0300

    feat: update api config and clean up

--- config/api.config.js
+++ config/api.config.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  DB: process.env.DB,
-  SECRET: process.env.SECRET,
   ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN | "*",
   NODE_ENV: process.env.NODE_ENV || "Development",
-  LOCALE: process.env.LOCALE,
 };
